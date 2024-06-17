a858c88a0ff241d1408d9a3d066f0a8cdcea8e22
aumaphitep <aumaphitep@gmail.com>
2024-06-17 15:46:02 +0700

    add news

--- app/Livewire/ManageNews.php
+++ app/Livewire/ManageNews.php
@@ -6,6 +6,9 @@ use Livewire\Component;
 
 class ManageNews extends Component
 {
+    public function addNews(){
+        dd(555555);
+    }
     public function render()
     {
         return view('livewire.manage-news');
