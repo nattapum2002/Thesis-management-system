e45627c0ef5bd322dbbef85aa50b76ca97334576
Supachai888 <supachai.th@rmuti.ac.th>
2024-07-05 14:44:09 +0700

    comit

--- resources/views/livewire/news/manage-news.blade.php
+++ resources/views/livewire/news/manage-news.blade.php
@@ -2,13 +2,13 @@
     <div class="row">
         <div class="col-md-6">
             <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="Search" wire:model.live.debounce.150ms="search">
+                <input type="text" class="form-control" placeholder="Search" wire:model.debounce.150ms="search">
                 <button class="btn btn-primary" type="submit"><i class='bx bx-search'></i></button>
             </div>
         </div>
         <div class="col-sm-3">
             <div class="input-group mb-3">
-                <select class="form-select" wire:model.live.debounce.100ms="filterDate">
+                <select class="form-select" wire:model.debounce.100ms="filterDate">
                     <option value="ข่าวล่าสุด">ข่าวล่าสุด</option>
                     <option value="ข่าวเก่าสุด">ข่าวเก่าสุด</option>
                 </select>
@@ -16,7 +16,7 @@
         </div>
         <div class="col-sm-3">
             <div class="input-group mb-3">
-                <select class="form-select" wire:model.live.debounce.100ms="filterType">
+                <select class="form-select" wire:model.debounce.100ms="filterType">
                     <option value="ทุกประเภท">ทุกประเภท</option>
                     <option value="ข่าวทั่วไป">ข่าวทั่วไป</option>
                     <option value="ชื่อหัวข้อ">ชื่อหัวข้อ</option>
@@ -58,10 +58,10 @@
                         <td>{{ $news_detail->updated_at }}</td>
                         <td>
                             @if ($news_detail->status == '1')
-                            <a wire:click.live='show({{ $news_detail->id_news }})' class="btn btn-success"><i
+                            <a wire:click='show({{ $news_detail->id_news }})' class="btn btn-success"><i
                                     class='bx bxs-show'></i></a>
                             @else
-                            <a wire:click.live='hide({{ $news_detail->id_news }})' class="btn btn-danger"><i
+                            <a wire:click='hide({{ $news_detail->id_news }})' class="btn btn-danger"><i
                                     class='bx bxs-hide'></i></a>
                             @endif
                             @if ($users->user_type == 'Admin')
