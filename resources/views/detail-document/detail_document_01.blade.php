8aa9ed31620b8aef197b6727d8dc1bb44fbfb77e
nattapum <nattapum.kh@rmuti.ac.th>
2024-09-22 16:41:03 +0700

    navigation

--- resources/views/detail-document/detail_document_01.blade.php
+++ resources/views/detail-document/detail_document_01.blade.php
@@ -4,6 +4,12 @@
         'Admin' => 'layout.admin',
         default => 'layout.default', // Provide a default layout if needed
     };
+    $route = match (Auth::guard('teachers')->user()->user_type) {
+        'Branch head' => 'branch-head.approve.documents',
+        'Admin' => 'admin.approve.documents',
+        'Teacher' => 'teacher.approve.documents',
+        default => null,
+    };
 @endphp
 
 @extends($layout)
@@ -12,14 +18,11 @@
     รายละเอียดเอกสาร
 @endsection
 @section('navigation')
-    <li class="breadcrumb-item"><a href="{{ route('admin.approve.documents') }}">อนุมัติเอกสาร</a></li>
+    <li class="breadcrumb-item"><a href="{{ route($route) }}">อนุมัติเอกสาร</a></li>
 @endsection
 @section('content')
     <livewire:document-layout.document-head :id_project="$id_project" :id_document="$id_document" />
     @livewire('document-detail.document01', ['id_project' => $id_project, 'id_document' => $id_document])
-    @if (Auth::guard('teachers')->user()->user_type == 'Branch head')
-        <a href="{{ route('branch-head.approve.documents') }}" class="btn btn-orange mt-3 mb-3">ย้อนกลับ</a>
-    @elseif(Auth::guard('teachers')->user()->user_type == 'Admin')
-        <a href="{{ route('admin.approve.documents') }}" class="btn btn-orange mt-3 mb-3">ย้อนกลับ</a>
-    @endif
+
+    <a href="{{ route($route) }}" class="btn btn-orange mt-3 mb-3">ย้อนกลับ</a>
 @endsection
--- resources/views/detail-document/detail_document_04.blade.php
+++ resources/views/detail-document/detail_document_04.blade.php
@@ -4,14 +4,27 @@
         'Admin' => 'layout.admin',
         default => 'layout.default', // Provide a default layout if needed
     };
+    $route = match (Auth::guard('teachers')->user()->user_type) {
+        'Branch head' => 'branch-head.approve.documents',
+        'Admin' => 'admin.approve.documents',
+        'Teacher' => 'teacher.approve.documents',
+        default => null,
+    };
 @endphp
 
 @extends($layout)
-@section('title')
-รายละเอียดเอกสาร
-@endsection
-@section('content')
-<livewire:document-layout.document-head :id_project="$id_project" :id_document="$id_document" />
-@livewire('document-detail.document04',['id_project' => $id_project ,'id_document' => $id_document])
 
+@section('title')
+    รายละเอียดเอกสาร
+@endsection
+
+@section('navigation')
+    <li class="breadcrumb-item"><a href="{{ route($route) }}">อนุมัติเอกสาร</a></li>
+@endsection
+
+@section('content')
+    <livewire:document-layout.document-head :id_project="$id_project" :id_document="$id_document" />
+    @livewire('document-detail.document04', ['id_project' => $id_project, 'id_document' => $id_document])
+
+    <a href="{{ route($route) }}" class="btn btn-orange mt-3 mb-3">ย้อนกลับ</a>
 @endsection
